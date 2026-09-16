c883f7e25fd9aed00277e85f5245c5e3063e775e
dev-anitha <anithayadav989@gmail.com>
2026-09-16 10:30:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let express=require('express');
+let hrroutes=require('./routes/hr_routes')
+let app=express();
+
+app.use("/api/hr",hrroutes);
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000")
+
+})
+
+
---
+++ routes/hr_routes.js
@@ -0,0 +1,20 @@
+let express=require('express');
+let router=express.Router();
+
+router.get("/employees",(req,res)=>{
+    res.send("Employees called");
+
+});
+
+router.post("/assign-task",(req,res)=>{
+    res.send("assign task page called")
+});
+
+router.get("/tasks",(req,res)=>{
+    res.send("tasks called");
+});
+
+router.get("/notification",(req,res)=>{
+    res.send("notification called");
+})
+module.exports=router;
